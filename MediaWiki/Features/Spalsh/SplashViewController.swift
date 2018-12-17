5aaa203e73dff200f15cf4bdd6c2d46bf7ea52bc
Raunak <raunak.choudhary@box8.in>
2018-12-17 22:20:27 +0530

    :tada: Create Baseic Project Structure

---
+++ MediaWiki/Features/Spalsh/SplashViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SplashViewController.swift
+//  MediaWiki
+//
+//  Created by Raunak Choudhary on 17/12/18.
+//  Copyright © 2018 Raunak. All rights reserved.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
